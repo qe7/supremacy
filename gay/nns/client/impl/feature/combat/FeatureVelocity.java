0d256c8830d611ce36fcbabe8830d3059d203908
Ahruu <114790156+Ahruu@users.noreply.github.com>
2023-09-05 07:06:34 -0700

    Hypixel Velocity

--- gay/nns/client/impl/feature/combat/FeatureVelocity.java
+++ gay/nns/client/impl/feature/combat/FeatureVelocity.java
@@ -9,6 +9,7 @@ import gay.nns.client.api.setting.annotations.Mode;
 import gay.nns.client.api.setting.annotations.Serialize;
 import gay.nns.client.api.setting.annotations.Slider;
 import gay.nns.client.impl.event.packet.PacketReceiveEvent;
+import gay.nns.client.impl.event.player.MotionEvent;
 import gay.nns.client.impl.event.player.UpdateEvent;
 import gay.nns.client.impl.event.render.Render2DEvent;
 import gay.nns.client.util.IMinecraft;
@@ -23,7 +24,7 @@ import net.minecraft.util.EnumFacing;
 public class FeatureVelocity extends AbstractFeature {
 
     @Serialize(name = "Mode")
-    @Mode(modes = {"Standard", "Grim"})
+    @Mode(modes = {"Standard", "Grim", "Hypixel"})
     public String mode = "Standard";
 
     @Serialize(name = "Horizontal")
@@ -37,6 +38,11 @@ public class FeatureVelocity extends AbstractFeature {
     @Serialize(name = "Water_Check")
     @CheckBox
     public boolean waterCheck = true;
+
+    @Serialize(name = "Hypixel OnGround")
+    @CheckBox
+    public boolean onGroundCheck = false;
+    private boolean reset;
     private boolean realVelocity, tookVelocity;
 
     public FeatureVelocity() {
@@ -100,6 +106,24 @@ public class FeatureVelocity extends AbstractFeature {
 
                 }
             }
+
+            case "Hypixel": { //this should work
+                if (!(event.getPacket() instanceof S12PacketEntityVelocity s12)) {
+                    return;
+                }
+
+                if (s12.getEntityID() != mc.thePlayer.getEntityId()) {
+                    return;
+                }
+
+                if (mc.thePlayer.onGround && onGroundCheck) {
+                    reset = true;
+                    event.setCancelled(true);
+                    return;
+                }
+                event.setCancelled(true);
+                mc.thePlayer.motionY = s12.getMotionY() / 8000D;
+            }
         }
     }
 
@@ -112,6 +136,21 @@ public class FeatureVelocity extends AbstractFeature {
                 }
                 mc.getNetHandler().addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.STOP_DESTROY_BLOCK, new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ), EnumFacing.DOWN));
                 this.tookVelocity = false;
+            }
+        }
+    }
+
+    @Subscribe
+    public void playerMotionEvent(MotionEvent event) {
+        switch (mode) {
+            case "Hypixel": { //this should work
+                if (!reset) {
+                    return;
+                }
+
+                mc.thePlayer.motionX = 0;
+                mc.thePlayer.motionY = 0;
+                mc.thePlayer.motionZ = 0;
             }
         }
     }
