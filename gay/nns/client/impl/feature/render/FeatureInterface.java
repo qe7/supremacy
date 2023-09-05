397913387cb43664a426f55975537825440f7d7f
Ahruu <114790156+Ahruu@users.noreply.github.com>
2023-09-04 20:54:03 -0700

    added "(Suffix)" "[Suffix]" "- Suffix" " Suffix" Modes

--- gay/nns/client/impl/feature/render/FeatureInterface.java
+++ gay/nns/client/impl/feature/render/FeatureInterface.java
@@ -20,176 +20,210 @@ import java.util.*;
 @FeatureInfo(name = "Interface", description = "HUD", category = FeatureCategory.RENDER)
 public class FeatureInterface extends AbstractFeature {
 
-	@Serialize(name = "Arraylist_Color")
-	@ColorBox
-	public Color color = new Color(255, 255, 255);
+    @Serialize(name = "Arraylist_Color")
+    @ColorBox
+    public Color color = new Color(255, 255, 255);
 
-	@Serialize(name = "Background_Color")
-	@ColorBox
-	public Color backgroundColor = new Color(0, 0, 0, 100);
+    @Serialize(name = "Background_Color")
+    @ColorBox
+    public Color backgroundColor = new Color(0, 0, 0, 100);
 
-	@Serialize(name = "Color_Mode")
-	@Mode(modes = {"Default", "Category", "Rainbow", "Astolfo"})
-	public String colorMode = "Default";
+    @Serialize(name = "Color_Mode")
+    @Mode(modes = {"Default", "Category", "Rainbow", "Astolfo"})
+    public String colorMode = "Default";
 
-	@Serialize(name = "Arraylist_Mode")
-	@Mode(modes = {"Left", "Right", "Top", "Bottom", "All", "None"})
-	public String arraylistMode = "All";
+    @Serialize(name = "Arraylist_Mode")
+    @Mode(modes = {"Left", "Right", "Top", "Bottom", "All", "None"})
+    public String arraylistMode = "All";
 
-	@Serialize(name = "Arraylist_Brightness")
-	@Slider(min = 0, max = 1, increment = 0.05)
-	public double brightness = 0.7;
+    @Serialize(name = "Suffix_Mode")
+    @Mode(modes = {"Parenthesis", "Squared_Brackets", "Hyphen", "Space", "None"})
+    public String suffixMode = "Parenthesis";
 
-	@Serialize(name = "Arraylist_Saturation")
-	@Slider(min = 0, max = 1, increment = 0.05)
-	public double saturation = 0.5;
+    @Serialize(name = "Arraylist_Brightness")
+    @Slider(min = 0, max = 1, increment = 0.05)
+    public double brightness = 0.7;
 
-	@Serialize(name = "Offset")
-	@Slider(min = 0, max = 10, increment = 1)
-	public double offset = 1;
+    @Serialize(name = "Arraylist_Saturation")
+    @Slider(min = 0, max = 1, increment = 0.05)
+    public double saturation = 0.5;
 
-	@Serialize(name = "Spacing")
-	@Slider(min = 0, max = 10, increment = 1)
-	public double spacing = 2;
+    @Serialize(name = "Offset")
+    @Slider(min = 0, max = 10, increment = 1)
+    public double offset = 1;
 
-	@Serialize(name = "Background")
-	@CheckBox
-	public boolean background = true;
+    @Serialize(name = "Spacing")
+    @Slider(min = 0, max = 10, increment = 1)
+    public double spacing = 2;
 
-	public FeatureInterface() {
-		this.toggle();
-	}
+    @Serialize(name = "Background")
+    @CheckBox
+    public boolean background = true;
 
-	@Subscribe
-	public void onRender2D(final Render2DEvent render2DEvent) {
-		if (mc.theWorld == null) return;
-		if (mc.thePlayer == null) return;
+    public FeatureInterface() {
+        this.toggle();
+    }
 
-		List<AbstractFeature> featureList = new ArrayList<>(Core.getSingleton().getFeatureManager().getEnabledFeatures());
-		ScaledResolution sr = render2DEvent.getScaledResolution();
-		FontRenderer fr = mc.fontRendererObj;
+    @Subscribe
+    public void onRender2D(final Render2DEvent render2DEvent) {
+        if (mc.theWorld == null) return;
+        if (mc.thePlayer == null) return;
 
-		this.setSuffix("Default");
+        List<AbstractFeature> featureList = new ArrayList<>(Core.getSingleton().getFeatureManager().getEnabledFeatures());
+        ScaledResolution sr = render2DEvent.getScaledResolution();
+        FontRenderer fr = mc.fontRendererObj;
 
-		String time = new SimpleDateFormat("hh:mm a").format(new Date());
-		if (time.startsWith("0")) {
-			time = time.replaceFirst("0", "");
-		}
-		String watermark = Core.getSingleton().getName() + " §w" + Core.getSingleton().getVersion() + " §7(" + time.toUpperCase() + ")";
+        this.setSuffix("Default");
 
-		fr.drawStringWithShadow(watermark, 2.f, 2.f, getColor().getRGB());
+        String time = new SimpleDateFormat("hh:mm a").format(new Date());
+        if (time.startsWith("0")) {
+            time = time.replaceFirst("0", "");
+        }
+        String watermark = Core.getSingleton().getName() + " §w" + Core.getSingleton().getVersion() + " §7(" + time.toUpperCase() + ")";
 
-		featureList.sort(Comparator.comparingInt(m -> fr.getStringWidth(m.getSuffix() != null ? m.getFeatureInfo().name().replace("_", " ") + " §7(" + m.getSuffix() + ")" : m.getFeatureInfo().name().replace("_", " "))));
-		Collections.reverse(featureList);
+        fr.drawStringWithShadow(watermark, 2.f, 2.f, getColor().getRGB());
 
-		double lastLength = 0;
-		double y = offset;
-		for (AbstractFeature feature : featureList) {
-			if (feature.isEnabled()) {
-				String arraylist = feature.getFeatureInfo().name();
+        switch (suffixMode) {
+            case "Parenthesis" -> {
+                featureList.sort(Comparator.comparingInt(m -> fr.getStringWidth(m.getSuffix() != null ? m.getFeatureInfo().name().replace("_", " ") + " §7(" + m.getSuffix() + ")" : m.getFeatureInfo().name().replace("_", " "))));
+                Collections.reverse(featureList);
+            }
+            case "Squared_Brackets" -> {
+                featureList.sort(Comparator.comparingInt(m -> fr.getStringWidth(m.getSuffix() != null ? m.getFeatureInfo().name().replace("_", " ") + " §7[" + m.getSuffix() + "]" : m.getFeatureInfo().name().replace("_", " "))));
+                Collections.reverse(featureList);
+            }
+            case "Hyphen" -> {
+                featureList.sort(Comparator.comparingInt(m -> fr.getStringWidth(m.getSuffix() != null ? m.getFeatureInfo().name().replace("_", " ") + " §7- " + m.getSuffix(): m.getFeatureInfo().name().replace("_", " "))));
+                Collections.reverse(featureList);
+            }
+            case "Space" -> {
+                featureList.sort(Comparator.comparingInt(m -> fr.getStringWidth(m.getSuffix() != null ? m.getFeatureInfo().name().replace("_", " ") + " §7" + m.getSuffix(): m.getFeatureInfo().name().replace("_", " "))));
+                Collections.reverse(featureList);
+            }
+            case "None" -> {
+                featureList.sort(Comparator.comparingInt(m -> fr.getStringWidth(m.getFeatureInfo().name().replace("_", " "))));
+                Collections.reverse(featureList);
+            }
+        }
 
-				if (feature.getSuffix() != null)
-					arraylist += " §7(" + feature.getSuffix() + ")";
+        double lastLength = 0;
+        double y = offset;
+        for (AbstractFeature feature : featureList) {
+            if (feature.isEnabled()) {
+                String arraylist = feature.getFeatureInfo().name();
 
-				arraylist = arraylist.replace("_", " ");
+                if (feature.getSuffix() != null)
+                    switch (suffixMode) {
+                        case "Parenthesis" -> arraylist += " §7(" + feature.getSuffix() + ")";
+                        case "Squared_Brackets" -> arraylist += " §7[" + feature.getSuffix() + "]";
+                        case "Hyphen" -> arraylist += " §7- " + feature.getSuffix();
+                        case "Space" -> arraylist += " §7" + feature.getSuffix();
+                        case "None" -> arraylist += "";
 
-				if (background)
-					Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y - 1), (float) ((float) sr.getScaledWidth() - offset + 1), (float) (y + fr.FONT_HEIGHT + spacing - 1), new Color(backgroundColor.getRed(), backgroundColor.getGreen(), backgroundColor.getBlue(), backgroundColor.getAlpha()).getRGB());
 
-				switch (arraylistMode.toLowerCase()) {
-					case "left" -> Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 2), (float) (y - 1), (float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y + fr.FONT_HEIGHT + spacing - 1), getColor(feature, (int) y * 10).getRGB());
-					case "right" -> Gui.drawRect((float) (sr.getScaledWidth() - offset + 1), (float) (y - 1), (float) ((float) sr.getScaledWidth() - offset + 2), (float) (y + fr.FONT_HEIGHT + spacing - 1), getColor(feature, (int) y * 10).getRGB());
-					case "top" -> {
-						if (featureList.indexOf(feature) == 0)
-							Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y - 2), (float) ((float) sr.getScaledWidth() - offset + 1), (float) (y - 1), getColor(feature, (int) y * 10).getRGB());
-					}
-					case "bottom" -> {
-						if (featureList.indexOf(feature) == featureList.size() - 1)
-							Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y + fr.FONT_HEIGHT + spacing - 1), (float) ((float) sr.getScaledWidth() - offset + 1), (float) (y + fr.FONT_HEIGHT + spacing), getColor(feature, (int) y * 10).getRGB());
-					}
-					case "all" -> {
-						// top
-						if (featureList.indexOf(feature) == 0)
-							Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 2), (float) (y - 2), (float) ((float) sr.getScaledWidth() - offset + 2), (float) (y - 1), getColor(feature, (int) y * 10).getRGB());
-						// bottom
-						if (featureList.indexOf(feature) == featureList.size() - 1)
-							Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 2), (float) (y + fr.FONT_HEIGHT + spacing - 1), (float) ((float) sr.getScaledWidth() - offset + 2), (float) (y + fr.FONT_HEIGHT + spacing), getColor(feature, (int) y * 10).getRGB());
-						// left
-						Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 2), (float) (y - 1), (float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y + fr.FONT_HEIGHT + spacing - 1), getColor(feature, (int) y * 10).getRGB());
-						// right
-						Gui.drawRect((float) (sr.getScaledWidth() - offset + 1), (float) (y - 1), (float) ((float) sr.getScaledWidth() - offset + 2), (float) (y + fr.FONT_HEIGHT + spacing - 1), getColor(feature, (int) y * 10).getRGB());
-						// middle? ig lol idfk
-						if (featureList.indexOf(feature) != 0)
-							Gui.drawRect((float) (sr.getScaledWidth() - lastLength - offset - 2), (float) (y - 1), (float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y), getColor(feature, (int) y * 10).getRGB());
-					}
-				}
+                    }
+                arraylist = arraylist.replace("_", " ");
 
-				fr.drawStringWithShadow(arraylist, (float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset), (float) ((float) y + (spacing / 2)), getColor(feature, (int) y * 10).getRGB());
-				lastLength = fr.getStringWidth(arraylist);
-				y += fr.FONT_HEIGHT + spacing;
-			}
-		}
-	}
 
-	@Subscribe
-	public void onKey(final KeyEvent keyEvent) {
-		// todo: handle TabUI key inputs
-	}
+                if (background)
+                    Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y - 1), (float) ((float) sr.getScaledWidth() - offset + 1), (float) (y + fr.FONT_HEIGHT + spacing - 1), new Color(backgroundColor.getRed(), backgroundColor.getGreen(), backgroundColor.getBlue(), backgroundColor.getAlpha()).getRGB());
 
-	private Color getColor(AbstractFeature feature, int index) {
-		switch (colorMode) {
-			case "Default" -> {
-				return color;
-			}
-			case "Category" -> {
-				return feature.getFeatureInfo().category().getColor();
-			}
-			case "Rainbow" -> {
-				return generateRainbowColor(index);
-			}
-			case "Astolfo" -> {
-				return generateAstolfoColor(index);
-			}
-		}
-		return Color.white;
-	}
+                switch (arraylistMode.toLowerCase()) {
+                    case "left" ->
+                            Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 2), (float) (y - 1), (float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y + fr.FONT_HEIGHT + spacing - 1), getColor(feature, (int) y * 10).getRGB());
+                    case "right" ->
+                            Gui.drawRect((float) (sr.getScaledWidth() - offset + 1), (float) (y - 1), (float) ((float) sr.getScaledWidth() - offset + 2), (float) (y + fr.FONT_HEIGHT + spacing - 1), getColor(feature, (int) y * 10).getRGB());
+                    case "top" -> {
+                        if (featureList.indexOf(feature) == 0)
+                            Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y - 2), (float) ((float) sr.getScaledWidth() - offset + 1), (float) (y - 1), getColor(feature, (int) y * 10).getRGB());
+                    }
+                    case "bottom" -> {
+                        if (featureList.indexOf(feature) == featureList.size() - 1)
+                            Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y + fr.FONT_HEIGHT + spacing - 1), (float) ((float) sr.getScaledWidth() - offset + 1), (float) (y + fr.FONT_HEIGHT + spacing), getColor(feature, (int) y * 10).getRGB());
+                    }
+                    case "all" -> {
+                        // top
+                        if (featureList.indexOf(feature) == 0)
+                            Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 2), (float) (y - 2), (float) ((float) sr.getScaledWidth() - offset + 2), (float) (y - 1), getColor(feature, (int) y * 10).getRGB());
+                        // bottom
+                        if (featureList.indexOf(feature) == featureList.size() - 1)
+                            Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 2), (float) (y + fr.FONT_HEIGHT + spacing - 1), (float) ((float) sr.getScaledWidth() - offset + 2), (float) (y + fr.FONT_HEIGHT + spacing), getColor(feature, (int) y * 10).getRGB());
+                        // left
+                        Gui.drawRect((float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 2), (float) (y - 1), (float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y + fr.FONT_HEIGHT + spacing - 1), getColor(feature, (int) y * 10).getRGB());
+                        // right
+                        Gui.drawRect((float) (sr.getScaledWidth() - offset + 1), (float) (y - 1), (float) ((float) sr.getScaledWidth() - offset + 2), (float) (y + fr.FONT_HEIGHT + spacing - 1), getColor(feature, (int) y * 10).getRGB());
+                        // middle? ig lol idfk
+                        if (featureList.indexOf(feature) != 0)
+                            Gui.drawRect((float) (sr.getScaledWidth() - lastLength - offset - 2), (float) (y - 1), (float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset - 1), (float) (y), getColor(feature, (int) y * 10).getRGB());
+                    }
+                }
 
-	private Color getColor() {
-		switch (colorMode) {
-			case "Default" -> {
-				return color;
-			}
-			case "Category" -> {
-				return Color.white;
-			}
-			case "Rainbow" -> {
-				return generateRainbowColor(1);
-			}
-			case "Astolfo" -> {
-				return generateAstolfoColor(1);
-			}
-		}
-		return Color.white;
-	}
+                fr.drawStringWithShadow(arraylist, (float) (sr.getScaledWidth() - fr.getStringWidth(arraylist) - offset), (float) ((float) y + (spacing / 2)), getColor(feature, (int) y * 10).getRGB());
+                lastLength = fr.getStringWidth(arraylist);
+                y += fr.FONT_HEIGHT + spacing;
+            }
+        }
+    }
 
-	private Color generateRainbowColor(int i) {
-		float hue = (System.currentTimeMillis() + i) % 4000;
-		hue /= 4000f;
-		return Color.getHSBColor(hue, (float) (saturation), (float) (brightness));
-	}
+    @Subscribe
+    public void onKey(final KeyEvent keyEvent) {
+        // todo: handle TabUI key inputs
+    }
 
-	private Color generateAstolfoColor(int i) {
-		float speed = 4000f;
-		float hue = (float) (System.currentTimeMillis() % 4000) + (i);
-		while (hue > speed) {
-			hue -= speed;
-		}
-		hue /= speed;
-		if (hue > 0.5) {
-			hue = 0.5f - (hue - 0.5f);
-		}
-		hue += 0.5f;
-		return Color.getHSBColor(hue, (float) (saturation), (float) (brightness));
-	}
+    private Color getColor(AbstractFeature feature, int index) {
+        switch (colorMode) {
+            case "Default" -> {
+                return color;
+            }
+            case "Category" -> {
+                return feature.getFeatureInfo().category().getColor();
+            }
+            case "Rainbow" -> {
+                return generateRainbowColor(index);
+            }
+            case "Astolfo" -> {
+                return generateAstolfoColor(index);
+            }
+        }
+        return Color.white;
+    }
+
+    private Color getColor() {
+        switch (colorMode) {
+            case "Default" -> {
+                return color;
+            }
+            case "Category" -> {
+                return Color.white;
+            }
+            case "Rainbow" -> {
+                return generateRainbowColor(1);
+            }
+            case "Astolfo" -> {
+                return generateAstolfoColor(1);
+            }
+        }
+        return Color.white;
+    }
+
+    private Color generateRainbowColor(int i) {
+        float hue = (System.currentTimeMillis() + i) % 4000;
+        hue /= 4000f;
+        return Color.getHSBColor(hue, (float) (saturation), (float) (brightness));
+    }
+
+    private Color generateAstolfoColor(int i) {
+        float speed = 4000f;
+        float hue = (float) (System.currentTimeMillis() % 4000) + (i);
+        while (hue > speed) {
+            hue -= speed;
+        }
+        hue /= speed;
+        if (hue > 0.5) {
+            hue = 0.5f - (hue - 0.5f);
+        }
+        hue += 0.5f;
+        return Color.getHSBColor(hue, (float) (saturation), (float) (brightness));
+    }
 
 }
