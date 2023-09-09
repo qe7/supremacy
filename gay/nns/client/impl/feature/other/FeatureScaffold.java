ed736af524269ea93bdbe1dcac218ae8f0114888
Shae <89613516+qe7@users.noreply.github.com>
2023-09-09 07:02:32 +0100

    Added cancer mode

--- gay/nns/client/impl/feature/other/FeatureScaffold.java
+++ gay/nns/client/impl/feature/other/FeatureScaffold.java
@@ -4,11 +4,10 @@ import gay.nns.client.api.core.Core;
 import gay.nns.client.api.event.interfaces.Subscribe;
 import gay.nns.client.api.feature.AbstractFeature;
 import gay.nns.client.api.feature.interfaces.FeatureInfo;
+import gay.nns.client.api.setting.annotations.Serialize;
 import gay.nns.client.api.setting.annotations.SettingBoolean;
 import gay.nns.client.api.setting.annotations.SettingMode;
-import gay.nns.client.api.setting.annotations.Serialize;
 import gay.nns.client.api.setting.annotations.SettingSlider;
-import gay.nns.client.impl.event.player.EventPostMotion;
 import gay.nns.client.impl.event.player.EventPreMotion;
 import gay.nns.client.impl.event.player.EventUpdate;
 import gay.nns.client.impl.event.render.EventRender2D;
@@ -28,7 +27,6 @@ import net.minecraft.util.Vec3;
 import org.lwjgl.input.Keyboard;
 
 import javax.vecmath.Vector2f;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -39,12 +37,16 @@ public class FeatureScaffold extends AbstractFeature {
 	@SettingBoolean
 	public static boolean safeWalk = true;
 
+	@Serialize(name = "Ray_Trace")
+	@SettingBoolean
+	public boolean rayTrace = true;
+
 	@Serialize(name = "Limit_Speed")
 	@SettingBoolean
 	public boolean limitSpeed = true;
 
 	@Serialize(name = "Mode")
-	@SettingMode(modes = {"Vanilla", "Hypixel"})
+	@SettingMode(modes = {"Vanilla", "Hypixel", "Cancer"})
 	public String mode = "Vanilla";
 
 	@Serialize(name = "Place_Delay")
@@ -57,14 +59,28 @@ public class FeatureScaffold extends AbstractFeature {
 
 	private final ChatUtil chatUtil = new ChatUtil();
 	private final TimerUtil timerUtil = new TimerUtil();
+	private final TimerUtil spinTimerUtil = new TimerUtil();
 
-	/* thank co-pilot for this, :) */
-	private final List<Block> badBlocks = Arrays.asList(Blocks.air, Blocks.water, Blocks.flowing_water, Blocks.lava, Blocks.flowing_lava, Blocks.enchanting_table, Blocks.carpet, Blocks.glass_pane, Blocks.stained_glass_pane, Blocks.iron_bars, Blocks.snow_layer, Blocks.ice, Blocks.packed_ice, Blocks.coal_ore, Blocks.diamond_ore, Blocks.emerald_ore, Blocks.chest, Blocks.trapped_chest, Blocks.torch, Blocks.anvil, Blocks.trapped_chest, Blocks.noteblock, Blocks.jukebox, Blocks.tnt, Blocks.gold_ore, Blocks.iron_ore, Blocks.lapis_ore, Blocks.lit_redstone_ore, Blocks.quartz_ore, Blocks.redstone_ore, Blocks.wooden_pressure_plate, Blocks.stone_pressure_plate, Blocks.light_weighted_pressure_plate, Blocks.heavy_weighted_pressure_plate, Blocks.stone_button, Blocks.wooden_button, Blocks.lever, Blocks.tallgrass, Blocks.tripwire, Blocks.tripwire_hook, Blocks.rail, Blocks.waterlily, Blocks.red_flower, Blocks.red_mushroom, Blocks.brown_mushroom, Blocks.vine, Blocks.trapdoor, Blocks.yellow_flower, Blocks.ladder, Blocks.furnace, Blocks.sand, Blocks.cactus, Blocks.dispenser, Blocks.noteblock, Blocks.dropper, Blocks.crafting_table, Blocks.web, Blocks.pumpkin, Blocks.sapling, Blocks.cobblestone_wall, Blocks.oak_fence, Blocks.beacon), invalidBlocks = Arrays.asList(Blocks.air, Blocks.water, Blocks.fire, Blocks.flowing_water, Blocks.lava, Blocks.flowing_lava);
+	private final List<Block> badBlocks = List.of(
+			Blocks.air, Blocks.water, Blocks.flowing_water, Blocks.lava, Blocks.flowing_lava,
+			Blocks.enchanting_table, Blocks.carpet, Blocks.glass_pane, Blocks.stained_glass_pane,
+			Blocks.iron_bars, Blocks.snow_layer, Blocks.ice, Blocks.packed_ice, Blocks.coal_ore,
+			Blocks.diamond_ore, Blocks.emerald_ore, Blocks.chest, Blocks.trapped_chest, Blocks.torch,
+			Blocks.anvil, Blocks.trapped_chest, Blocks.noteblock, Blocks.jukebox, Blocks.tnt, Blocks.gold_ore,
+			Blocks.iron_ore, Blocks.lapis_ore, Blocks.lit_redstone_ore, Blocks.quartz_ore, Blocks.redstone_ore,
+			Blocks.wooden_pressure_plate, Blocks.stone_pressure_plate, Blocks.light_weighted_pressure_plate,
+			Blocks.heavy_weighted_pressure_plate, Blocks.stone_button, Blocks.wooden_button, Blocks.lever,
+			Blocks.tallgrass, Blocks.tripwire, Blocks.tripwire_hook, Blocks.rail, Blocks.waterlily,
+			Blocks.red_flower, Blocks.red_mushroom, Blocks.brown_mushroom, Blocks.vine, Blocks.trapdoor,
+			Blocks.yellow_flower, Blocks.ladder, Blocks.furnace, Blocks.sand, Blocks.cactus, Blocks.dispenser,
+			Blocks.noteblock, Blocks.dropper, Blocks.crafting_table, Blocks.web, Blocks.pumpkin, Blocks.sapling,
+			Blocks.cobblestone_wall, Blocks.oak_fence, Blocks.beacon);
+
+	private final List<Block> invalidBlocks = List.of(
+			Blocks.air, Blocks.water, Blocks.fire, Blocks.flowing_water, Blocks.lava, Blocks.flowing_lava, Blocks.tallgrass);
 
 	private BlockData blockData, prevBlockData;
-
 	private float yaw, pitch;
-
 	private int oldSlot = -1;
 
 	public FeatureScaffold() {
@@ -74,24 +90,21 @@ public class FeatureScaffold extends AbstractFeature {
 	@Override
 	protected void onEnable() {
 		super.onEnable();
-
 		oldSlot = mc.thePlayer.inventory.currentItem;
 	}
 
 	@Override
 	protected void onDisable() {
 		super.onDisable();
-
 		mc.thePlayer.inventory.currentItem = oldSlot;
 	}
 
 	@Subscribe
 	public void onRender2D(final EventRender2D render2DEvent) {
 		FontRenderer fr = mc.fontRendererObj;
-
 		String blocks = "blocks: " + getBlockCount() + " | " + getBlockSlot();
-
-		fr.drawStringWithShadow(blocks, (float) mc.displayWidth / 4 - ((float) fr.getStringWidth(blocks) / 2), (float) mc.displayHeight / 4 + 10, -1);
+		fr.drawStringWithShadow(blocks, (float) mc.displayWidth / 4 - (fr.getStringWidth(blocks) / 2f),
+				(float) mc.displayHeight / 4 + 10, -1);
 	}
 
 	@Subscribe
@@ -104,22 +117,17 @@ public class FeatureScaffold extends AbstractFeature {
 
 	@Subscribe
 	public void onPreMotion(final EventPreMotion preMotionEvent) {
-
 		BlockPos blockUnder = new BlockPos(mc.thePlayer.posX, mc.thePlayer.posY - 1.0, mc.thePlayer.posZ);
 		blockData = getBlockData(blockUnder);
-
 		if (blockData == null) {
 			return;
 		}
-
 		if (prevBlockData == null) {
 			prevBlockData = blockData;
 		}
-
-		if (prevBlockData != blockData) {
+		if (!Objects.equals(prevBlockData, blockData)) {
 			prevBlockData = blockData;
 		}
-
 		pitch = MathUtil.getRandom(80.0F, 82.0F);
 		switch (mode.toLowerCase()) {
 			case "vanilla" -> {
@@ -135,53 +143,49 @@ public class FeatureScaffold extends AbstractFeature {
 				}
 			}
 			case "hypixel" -> {
-				// Calculate the yaw and pitch needed to look at the block, these should use as legit values as possible
-
-				yaw = mc.thePlayer.rotationYaw - MathUtil.getRandom(178.0F, 182.0F);;
+				yaw = mc.thePlayer.rotationYaw - MathUtil.getRandom(178.0F, 182.0F);
 				if (Objects.requireNonNull(blockData.getFacing()) == EnumFacing.UP) {
 					yaw = mc.thePlayer.rotationYaw;
 					pitch = 90.0F;
 				}
 			}
+			case "cancer" -> {
+				switch (blockData.getFacing()) {
+					case NORTH -> yaw = 180.0F;
+					case EAST -> yaw = 270.0F;
+					case SOUTH -> yaw = 0.0F;
+					case WEST -> yaw = 90.0F;
+					case UP -> {
+						yaw = MathUtil.getRandom(-180.0F, 180.0F);
+						pitch = 90.0f;
+					}
+				}
+			}
 		}
-
 		Vector2f rotations = new Vector2f(yaw, pitch);
 		Vector2f smoothRotations = RotationUtil.getSmoothRotations(mc.thePlayer.getPreviousRotation(), rotations, rotationSpeed);
-
 		smoothRotations = RotationUtil.applySanity(smoothRotations);
-
 		smoothRotations = RotationUtil.applyGCD(smoothRotations);
-
 		Core.getSingleton().getRotationManager().setRotation(smoothRotations);
-
-		// get current speed and set to around 10% less
-		if (limitSpeed && mc.thePlayer.onGround && !Keyboard.isKeyDown(mc.gameSettings.keyBindJump.getKeyCode()) && Keyboard.isKeyDown(mc.gameSettings.keyBindForward.getKeyCode())) {
+		if (limitSpeed && mc.thePlayer.onGround && !Keyboard.isKeyDown(mc.gameSettings.keyBindJump.getKeyCode()) &&
+				Keyboard.isKeyDown(mc.gameSettings.keyBindForward.getKeyCode())) {
 			double currentSpeed = Math.sqrt(mc.thePlayer.motionX * mc.thePlayer.motionX + mc.thePlayer.motionZ * mc.thePlayer.motionZ);
 			double speed = currentSpeed * 0.95D;
-
-			// get direction
 			float direction = mc.thePlayer.rotationYaw;
-
-			// get motion
 			double motionX = -Math.sin(direction / 180.0F * Math.PI) * speed;
 			double motionZ = Math.cos(direction / 180.0F * Math.PI) * speed;
-
-			// set motion
 			mc.thePlayer.motionX = motionX;
 			mc.thePlayer.motionZ = motionZ;
 		}
-
-		if (!rayCast(yaw, pitch)) {
+		if (rayTrace && !rayCast(yaw, pitch)) {
 			timerUtil.reset();
 			return;
 		}
-
 		if (timerUtil.hasTimeElapsed(MathUtil.getRandom((int) placeDelay, (int) placeDelay + 10))) {
-			if (getBlockSlot() != -1 && rayCast(yaw, pitch)) {
+			if (getBlockSlot() != -1) {
 				if (mc.thePlayer.inventory.currentItem != getBlockSlot()) {
 					mc.thePlayer.inventory.currentItem = getBlockSlot();
 				}
-
 				if (mc.playerController.onPlayerRightClick(
 						mc.thePlayer,
 						mc.theWorld,
@@ -197,39 +201,21 @@ public class FeatureScaffold extends AbstractFeature {
 		}
 	}
 
-	@Subscribe
-	public void onPostMotion(final EventPostMotion postMotionEvent) {
-
-	}
-
 	private Vec3 getVectorForRotation(float yaw, float pitch) {
-		// Calculate trigonometric values for yaw and pitch
 		double cosYaw = Math.cos(Math.toRadians(-yaw) - Math.PI);
 		double sinYaw = Math.sin(Math.toRadians(-yaw) - Math.PI);
 		double cosPitch = -Math.cos(Math.toRadians(-pitch));
 		double sinPitch = Math.sin(Math.toRadians(-pitch));
-
-		// Calculate and return the resulting vector
 		return new Vec3(sinYaw * cosPitch, sinPitch, cosYaw * cosPitch);
 	}
 
 	private boolean rayCast(float yaw, float pitch) {
-		// Get the player's eye position
 		Vec3 eyePosition = mc.thePlayer.getPositionEyes(1.0F);
-
-		// Calculate the direction vector based on yaw and pitch
 		Vec3 directionVector = getVectorForRotation(yaw, pitch);
-
-		// Calculate the endpoint of the ray-cast
 		Vec3 rayEndPoint = eyePosition.addVector(directionVector.xCoord * 5.0, directionVector.yCoord * 5.0, directionVector.zCoord * 5.0);
-
-		// Perform the ray-cast
 		MovingObjectPosition result = mc.theWorld.rayTraceBlocks(eyePosition, rayEndPoint, false);
-
-		// Check if the ray-cast hit a block and if it's the expected block position
 		return result != null && result.typeOfHit == MovingObjectPosition.MovingObjectType.BLOCK && blockData.getPosition().equals(result.getBlockPos());
 	}
-
 
 	private boolean isPosSolid(BlockPos pos) {
 		Block block = mc.theWorld.getBlockState(pos).getBlock();
@@ -237,19 +223,18 @@ public class FeatureScaffold extends AbstractFeature {
 	}
 
 	private int getBlockSlot() {
-		for(int k = 0; k < 9; ++k) {
+		for (int k = 0; k < 9; ++k) {
 			ItemStack itemStack = mc.thePlayer.inventory.mainInventory[k];
 			if (itemStack != null && isValid(itemStack) && itemStack.stackSize >= 1) {
 				return k;
 			}
 		}
-
 		return -1;
 	}
 
 	private boolean isValid(ItemStack itemStack) {
 		if (itemStack.getItem() instanceof ItemBlock) {
-			return !badBlocks.contains(((ItemBlock)itemStack.getItem()).getBlock());
+			return !badBlocks.contains(((ItemBlock) itemStack.getItem()).getBlock());
 		} else {
 			return false;
 		}
@@ -257,30 +242,26 @@ public class FeatureScaffold extends AbstractFeature {
 
 	private int getBlockCount() {
 		int count = 0;
-
-		for(int k = 0; k < mc.thePlayer.inventory.mainInventory.length; ++k) {
-			ItemStack itemStack = mc.thePlayer.inventory.mainInventory[k];
+		for (ItemStack itemStack : mc.thePlayer.inventory.mainInventory) {
 			if (itemStack != null && isValid(itemStack) && itemStack.stackSize >= 1) {
 				count += itemStack.stackSize;
 			}
 		}
-
 		return count;
 	}
 
 	private BlockData getBlockData(BlockPos pos) {
 		BlockPos[] offsets = {
-				new BlockPos(0, -1, 0),   // Below
-				new BlockPos(-1, 0, 0),  // East
-				new BlockPos(1, 0, 0),   // West
-				new BlockPos(0, 0, 1),   // North
-				new BlockPos(0, 0, -1),  // South
-				new BlockPos(-1, 0, -1), // South-East
-				new BlockPos(1, 0, -1),  // South-West
-				new BlockPos(-1, 0, 1),  // North-East
-				new BlockPos(1, 0, 1)    // North-West
+				new BlockPos(0, -1, 0),
+				new BlockPos(-1, 0, 0),
+				new BlockPos(1, 0, 0),
+				new BlockPos(0, 0, 1),
+				new BlockPos(0, 0, -1),
+				new BlockPos(-1, 0, -1),
+				new BlockPos(1, 0, -1),
+				new BlockPos(-1, 0, 1),
+				new BlockPos(1, 0, 1)
 		};
-
 		for (BlockPos offset : offsets) {
 			BlockPos adjacentPos = pos.add(offset);
 			if (isPosSolid(adjacentPos)) {
@@ -305,18 +286,14 @@ public class FeatureScaffold extends AbstractFeature {
 				}
 			}
 		}
-
-		return null; // Return null if no solid block is found in adjacent positions.
+		return null;
 	}
 
-
-
 	public Vec3 getHitVec() {
-		/* Correct HitVec */
-		Vec3 hitVec = new Vec3(blockData.getPosition().getX() + Math.random(), blockData.getPosition().getY() + Math.random(), blockData.getPosition().getZ() + Math.random());
-
-		final MovingObjectPosition movingObjectPosition = rayCast(yaw, pitch) ? mc.theWorld.rayTraceBlocks(mc.thePlayer.getPositionEyes(1.0F), hitVec, false, false, true) : null;
-
+		Vec3 hitVec = new Vec3(blockData.getPosition().getX() + Math.random(),
+				blockData.getPosition().getY() + Math.random(), blockData.getPosition().getZ() + Math.random());
+		final MovingObjectPosition movingObjectPosition = rayCast(yaw, pitch) ?
+				mc.theWorld.rayTraceBlocks(mc.thePlayer.getPositionEyes(1.0F), hitVec, false, false, true) : null;
 		switch (blockData.enumFacing) {
 			case DOWN -> hitVec.yCoord = blockData.getPosition().getY();
 			case UP -> hitVec.yCoord = blockData.getPosition().getY() + 1;
@@ -325,12 +302,10 @@ public class FeatureScaffold extends AbstractFeature {
 			case SOUTH -> hitVec.zCoord = blockData.getPosition().getZ() + 1;
 			case WEST -> hitVec.xCoord = blockData.getPosition().getX();
 		}
-
 		if (movingObjectPosition != null && movingObjectPosition.getBlockPos().equals(blockData.getPosition()) &&
 				movingObjectPosition.sideHit == blockData.enumFacing) {
 			hitVec = movingObjectPosition.hitVec;
 		}
-
 		return hitVec;
 	}
 
@@ -351,5 +326,4 @@ public class FeatureScaffold extends AbstractFeature {
 			return blockPos;
 		}
 	}
-
 }
